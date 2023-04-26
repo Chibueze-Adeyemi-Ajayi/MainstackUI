c6170fd4320f8b80f9dc7e588b53e2b495c422a1
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-26 08:36:29 +0100

    MainStack UI

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 
-// jquery GET r
+// jquery GET re
 const sendResquest = (callback) => {
     $.get("https://fe-task-api.mainstack.io/", response => {
         callback(response);
